7446c9fa9afea1c38ceb2fbcbb07f200fae6a0ce
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-05 06:48:49 +0800

    Implement Grass class with attribute display method

--- java/Grass.java
+++ java/Grass.java
@@ -1,1 +1,12 @@
+public class Grass extends Metaclass {
+    @Override
+    public void displayAttributes() {
+        System.out.println("Grass Attributes:");
+        System.out.println("Height: 10cm");
+        System.out.println("Color: Green");
+        System.out.println("Density: Medium");
+        System.out.println("Growth Condition: Humid");
+        System.out.println("Interaction: Can be trampled or cut");
+    }
+}
 
