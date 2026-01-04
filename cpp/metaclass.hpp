2cf725446c3d0fdf175e3f47e1792a6b80ab7898
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-04 22:21:45 +0800

    Add Metaclass interface with displayAttributes method

--- cpp/metaclass.hpp
+++ cpp/metaclass.hpp
@@ -1,1 +1,12 @@
+// metaclass.hpp
+#ifndef METACLASS_HPP
+#define METACLASS_HPP
+
+class Metaclass {
+public:
+    virtual void displayAttributes() const = 0;
+    virtual ~Metaclass() = default;
+};
+
+#endif
 
