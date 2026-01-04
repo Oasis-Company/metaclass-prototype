6832d68ed495f9f3b8288de5e5fd4d1b503d4446
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-05 06:52:13 +0800

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,13 @@
+from grass import Grass
+from water import Water
+
+def main():
+    grass = Grass()
+    water = Water()
+
+    grass.display_attributes()
+    water.display_attributes()
+
+if __name__ == "__main__":
+    main()
 
