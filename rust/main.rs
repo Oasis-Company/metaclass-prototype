8ff2596c20c5b8e555bf9843df3bd25031d0c888
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-05 12:23:07 +0800

    Update main.rs

--- rust/main.rs
+++ rust/main.rs
@@ -1,1 +1,12 @@
+mod grass;
+mod water;
+mod metaclass;
+
+fn main() {
+    let grass = grass::Grass;
+    let water = water::Water;
+
+    grass.display_attributes();
+    water.display_attributes();
+}
 
